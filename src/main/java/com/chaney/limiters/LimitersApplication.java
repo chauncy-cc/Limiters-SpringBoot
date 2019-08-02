1162196ae5da7757daf30d989369c241b1477e29
chencheng <591626314@qq.com>
2019-08-02 15:42:08 +0800

    spring boot init

---
+++ src/main/java/com/chaney/limiters/LimitersApplication.java
@@ -0,0 +1,13 @@
+package com.chaney.limiters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chaney/limiters/LimitersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chaney.limiters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LimitersApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
