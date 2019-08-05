e15198dc8c751f107813de307554a3884afd7c88
chencheng <591626314@qq.com>
2019-08-05 21:33:51 +0800

    MyRateLimiter 令牌桶算法完成

--- src/main/java/com/chaney/limiters/limiters/MyRateLimiter.java
+++ src/main/java/com/chaney/limiters/limiters/MyRateLimiter.java
@@ -5,12 +5,27 @@ package com.chaney.limiters.limiters;
  */
 public class MyRateLimiter extends Limiter {
 
+    final int capacity;                             // 桶内能装多少令牌
+    int curTokenNum;                                // 现在桶内令牌数量
+    long lastTime;                                  // 时间戳
+
     MyRateLimiter(int qps) {
         super(qps);
+        capacity = qps;
+        curTokenNum = 0;
+        lastTime = 0;
     }
 
     @Override
     protected boolean tryAcquire() {
-        return false;
+        long now = System.currentTimeMillis();
+        int intoToken = (int)((now - lastTime)/1000.0 * capacity);
+        if (intoToken + curTokenNum > capacity) {
+            curTokenNum = capacity - 1;
+            return true;
+        } else if (intoToken + curTokenNum >= 1) {
+            curTokenNum = intoToken + curTokenNum - 1;
+            return true;
+        } else return false;
     }
 }
